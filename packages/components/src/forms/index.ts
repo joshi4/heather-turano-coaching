818a8938bde9bb11ab274a2c92a11f0eab33d063
Drew DeCarme <drewdecarme@gmail.com>
2020-01-24 15:06:13 -0500

    ci: Remove unneeded recaptcha component for build

--- packages/components/src/forms/index.ts
+++ packages/components/src/forms/index.ts
@@ -1,6 +1,5 @@
 export * from "./layouts/FormContainer";
 export * from "./layouts/InputGroup";
 export * from "./Honeypot";
-export * from "./ReCaptcha";
 export * from "./Input";
 export * from "./Textarea";
