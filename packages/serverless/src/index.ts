88b178901edea5366d0e5bedd115c3d3d8fefee1
Drew DeCarme <drewdecarme@gmail.com>
2020-02-19 07:59:57 -0500

    refactor(WIP): Add new api endpoint for serverless functions

--- packages/serverless/src/index.ts
+++ packages/serverless/src/index.ts
@@ -1,21 +1,22 @@
 /* eslint-disable no-restricted-globals */
 
-import { Router } from "./router";
-import { subscribeToBlog } from "./workers/subscribeToBlog.worker";
+// import { Router } from "./router";
+// import { subscribeToBlog } from "./workers/subscribeToBlog.worker";
 
 addEventListener("fetch", event => {
-  event.respondWith(handleRequest(event.request));
+  // event.respondWith(handleRequest(event.request));
+  event.respondWith(new Response("hello there"));
 });
 
 async function handleRequest(request: Request) {
-  const r = new Router();
-  r.post("/blog/subscribe", subscribeToBlog);
-  r.post("/blog/validate-email", subscribeToBlog);
-  //   r.get("/demos/router/foo", (req: Request) => fetch(req)); // return the response from the origin
+  // const r = new Router();
+  // r.post("/blog/subscribe", subscribeToBlog);
+  // r.post("/blog/validate-email", subscribeToBlog);
+  // //   r.get("/demos/router/foo", (req: Request) => fetch(req)); // return the response from the origin
 
-  //   r.get("/", async () => new Response("Hello worker!")); // return a default message for the root route
+  // //   r.get("/", async () => new Response("Hello worker!")); // return a default message for the root route
 
-  const resp = await r.route(request);
-  console.log(resp);
-  return new Response("testings!!");
+  // const resp = await r.route(request);
+  // console.log(resp);
+  return new Response("testings!");
 }
